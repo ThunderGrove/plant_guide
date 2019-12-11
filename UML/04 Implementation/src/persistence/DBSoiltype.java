ae7b19cf3820c0fa3ec72e348961717e789c6dbf
Emil Nymann <emilnymann96@gmail.com>
2019-12-11 12:48:45 +0100

    add DBSoiltype.editSoiltype() method

--- UML/04 Implementation/src/persistence/DBSoiltype.java
+++ UML/04 Implementation/src/persistence/DBSoiltype.java
@@ -23,4 +23,23 @@ public class DBSoiltype {
 
     }
 
+    public static void editSoiltype(Soiltype st) {
+
+        String query = "UPDATE soiltype SET name = ? " +
+                "WHERE id = ?";
+
+        try (Connection conn = DB.connect();
+            PreparedStatement ps = conn.prepareStatement(query)) {
+
+            ps.setString(1, st.getName());
+            ps.setInt(2, st.getId());
+
+            ps.executeUpdate();
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+
 }
