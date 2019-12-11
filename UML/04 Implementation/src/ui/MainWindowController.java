bf9d709ec3ee156b921ce9077fdf539ab482259b
Tommy Gultiano Hansen <54972963+EWCMF@users.noreply.github.com>
2019-12-11 13:56:50 +0100

    GUI additions
    Added controllers for other windows.

--- UML/04 Implementation/src/ui/MainWindowController.java
+++ UML/04 Implementation/src/ui/MainWindowController.java
@@ -18,7 +18,21 @@ public class MainWindowController {
         Stage stage = new Stage();
 
         stage.setScene(scene);
-        stage.setTitle("Create plant");
+        stage.setTitle("Ny plante");
+        stage.setResizable(false);
+        stage.initOwner(Main.window);
+        stage.initModality(Modality.WINDOW_MODAL);
+        stage.show();
+    }
+
+    @FXML
+    public void moveToEditWindow() throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("Plant Guide-4.fxml"));
+        Scene scene = new Scene(root);
+        Stage stage = new Stage();
+
+        stage.setScene(scene);
+        stage.setTitle("Rediger plante");
         stage.setResizable(false);
         stage.initOwner(Main.window);
         stage.initModality(Modality.WINDOW_MODAL);
