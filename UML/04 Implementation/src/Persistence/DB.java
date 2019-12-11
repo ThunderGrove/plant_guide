dbe56855a94a4a3000660bab35f3b1eaef462f3a
Emil Nymann <emilnymann96@gmail.com>
2019-12-11 09:35:40 +0100

    Add DB class with connect() method.

---
+++ UML/04 Implementation/src/Persistence/DB.java
@@ -0,0 +1,28 @@
+package Persistence;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DB {
+
+    public static Connection connect() {
+
+        Connection conn = null;
+
+        try  {
+            // db parameters
+            String url = "jdbc:sqlite:UML/04 Implementation/db/plantguide.db";
+            // create connection
+            conn = DriverManager.getConnection(url);
+
+            return conn;
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+
+            // will crash the program, maybe handle differently?
+            throw new RuntimeException("Connection to db failed.");
+        }
+    }
+
+}
